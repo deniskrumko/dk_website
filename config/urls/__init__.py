a7e485a18c321b7c2037dbf2e186a1607ded8086
Denis Krumko <dkrumko@gmail.com>
2018-05-13 18:59:01 +0300

    Completed index page (template)

--- config/urls/__init__.py
+++ config/urls/__init__.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-
+from django.views.generic.base import TemplateView
 admin.site.site_header = _('Denis Krumko')
 admin.site.site_title = _('DK')
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
 ]
