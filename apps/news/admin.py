f987fa448ede200c4d9b42571090e2ad86cf2833
Denis Krumko <dkrumko@gmail.com>
2018-09-03 15:46:53 +0300

    Disable likes. Noone uses them :(

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-
+from django.utils.translation import ugettext_lazy as _
 from .models import News
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     """Admin class for ``News`` model."""
+    fieldsets = (
+        (_('Main'), {
+            'fields': (
+                'title',
+                'text',
+                'date',
+            )
+        }),
+        (_('Optional'), {
+            'fields': (
+                'link',
+            )
+        }),
+    )
     list_display = (
         'title',
     )
