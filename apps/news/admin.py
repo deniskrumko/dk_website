89e0e06de8c754e104b8cf39d049020911b473e1
Denis Krumko <dkrumko@gmail.com>
2018-10-24 18:22:28 +0300

    Fix news admin

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+from django.http.response import HttpResponseRedirect
 
+from django.utils.translation import ugettext_lazy as _
+from django_object_actions import (
+    DjangoObjectActions,
+    takes_instance_or_queryset,
+)
 from .models import News
 
 
 @admin.register(News)
-class NewsAdmin(admin.ModelAdmin):
+class NewsAdmin(DjangoObjectActions, admin.ModelAdmin):
     """Admin class for ``News`` model."""
     fieldsets = (
         (_('Main'), {
             'fields': (
                 'image',
                 'title',
+                'slug',
                 'text',
                 'date',
             )
@@ -35,7 +41,41 @@ class NewsAdmin(admin.ModelAdmin):
     )
     list_display = (
         'title',
+        'slug',
+        'tab_title',
+        'date',
     )
     filter_horizontal = (
         'tags',
     )
+    readonly_fields = (
+        'slug',
+    )
+    change_actions = (
+        'reset_slug',
+        'on_site',
+    )
+    changelist_actions = (
+        'reset_slug',
+        'on_site',
+    )
+
+    @takes_instance_or_queryset
+    def reset_slug(self, request, qs):
+        """Action to reset `slug` field for ``Track`` objects."""
+        for obj in qs:
+            obj.slug = None
+            obj.save()
+
+    reset_slug.label = _('Reset slug')
+
+    @takes_instance_or_queryset
+    def on_site(self, request, qs=None):
+        """Method to view tracks or one track on site."""
+        if qs.count() > 1:
+            return HttpResponseRedirect('/news/')
+
+        item = qs.first()
+        return HttpResponseRedirect(f'/news/{item.slug}/')
+
+    on_site.label = _('View on site')
