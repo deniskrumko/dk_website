c69ac3f157fb1d4f73b23e0701d426c1214c9482
Denis Krumko <dkrumko@gmail.com>
2019-12-31 01:16:03 +0300

    Remove "diary top panel"

--- static/js/diary/diary.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  var element = document.getElementById("diary-tags");
-  if (currentScrollPos > 250) {
-    document.getElementById("diary-top").style.top = "0px";
-  } else {
-    document.getElementById("diary-top").style.top = "-280px";
-  }
-}
