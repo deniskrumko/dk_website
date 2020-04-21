6df5b0f5c73bc788c583a2e6fdac1c749f1a337e
Denis Krumko <dkrumko@gmail.com>
2020-04-21 19:33:03 +0300

    Fix usage of local.py

---
+++ config/settings/__init__.py
@@ -0,0 +1,7 @@
+from .common import *  # noqa
+
+try:
+    from .local import * # noqa
+except ModuleNotFoundError:
+    print('No local.py found. Using production.py')
+    from .production import * # noqa
