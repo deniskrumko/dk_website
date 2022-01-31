e92d1148013f9efcbedf2dc3ed37d4151a16cb64
Denis Krumko <dkrumko@gmail.com>
2022-01-31 18:49:23 +0300

    Remove RU translation

--- config/settings/common/locales.py
+++ config/settings/common/locales.py
@@ -1,20 +1,2 @@
-import os
-
-from django.utils.translation import ugettext_lazy as _
-
-from .paths import BASE_DIR
-
-LANGUAGE_CODE = 'ru-RU'
-LANGUAGES = [('ru', _('Russian'))]
-
 TIME_ZONE = 'Europe/Moscow'
-
-USE_I18N = True
-
-USE_L10N = True
-
 USE_TZ = True
-
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locales')
-]
